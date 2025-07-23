071e858e91d67414a701b173770ca98ca0bae3e5
Vivekkumar5697 <vivekkumarvodnala@gmail.com>
2025-07-23 22:28:57 +0000

    To Update backend with deployment CORS and middleware fix

--- utils/generateTokenAndCookie.js
+++ utils/generateTokenAndCookie.js
@@ -7,7 +7,8 @@ const generateTokenAndSetCookie = (res,userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
+    sameSite:"None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
